2979b4e1932eab8dc387395304d2a314b08d8ae2
stageor <STANLEY.IN.MANIT@GMAIL.COM>
2026-02-22 00:26:10 +0530

    Update Query 2.sql

--- Extra folder/Query 2.sql
+++ Extra folder/Query 2.sql
@@ -1,24 +1,32 @@
 WITH top_paying_jobs AS (
     SELECT
-        job_id,
+        job_postings_fact.job_id,
         job_title,
         salary_year_avg,
-        name AS company_name
+        company_dim.name AS company_name
     FROM
         job_postings_fact
-    LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+    LEFT JOIN company_dim 
+        ON job_postings_fact.company_id = company_dim.company_id
     WHERE
-        job_title_short = 'Data Analyst' 
-        AND job_location = 'Anywhere' 
+        job_title_short = 'Data Analyst'
+        AND job_location = 'Anywhere'
         AND salary_year_avg IS NOT NULL
     ORDER BY
         salary_year_avg DESC
-LIMIT 1000
+    LIMIT 1000
 )
 
 SELECT 
-    top_paying_jobs.*,
-    skills
-FROM top_paying_jobs
-INNER JOIN skills_job_dim ON top_paying_jobs.job_id = skills_job_dim.job_id
-INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+    tpj.job_id,
+    tpj.job_title,
+    tpj.company_name,
+    tpj.salary_year_avg,
+    skills_dim.skills
+FROM top_paying_jobs tpj
+INNER JOIN skills_job_dim 
+    ON tpj.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim 
+    ON skills_job_dim.skill_id = skills_dim.skill_id
+ORDER BY 
+    tpj.salary_year_avg DESC;

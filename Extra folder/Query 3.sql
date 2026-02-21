9b3d2480a244fb3b16957fa86a7263cde5f37a8e
stageor <STANLEY.IN.MANIT@GMAIL.COM>
2026-02-22 00:27:25 +0530

    Update Query 3.sql

--- Extra folder/Query 3.sql
+++ Extra folder/Query 3.sql
@@ -1,9 +1,13 @@
 SELECT
-    skills,
-    count(skills_job_dim.job_id) AS demand_count
+    skills_dim.skills,
+    COUNT(skills_job_dim.job_id) AS demand_count
 FROM job_postings_fact
-INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
-INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+INNER JOIN skills_job_dim 
+    ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim 
+    ON skills_job_dim.skill_id = skills_dim.skill_id
 GROUP BY
-    skills
-LIMIT 1000
+    skills_dim.skills
+ORDER BY
+    demand_count DESC
+LIMIT 1000;

fab8f4a4de1ba4bd88f5fa11bb7c3b81348736cb
stageor <STANLEY.IN.MANIT@GMAIL.COM>
2026-02-22 00:25:24 +0530

    Update Query 1.sql

--- Extra folder/Query 1.sql
+++ Extra folder/Query 1.sql
@@ -4,15 +4,16 @@ SELECT
     job_location,
     job_schedule_type,
     salary_year_avg,
-    job_posted_date
+    job_posted_date,
     company_name
 FROM
     job_postings_fact
-LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+LEFT JOIN company_dim 
+    ON job_postings_fact.company_id = company_dim.company_id
 WHERE
-    job_title_short = 'Data Analyst' 
-    AND job_location = 'Anywhere' 
+    job_title_short = 'Data Analyst'
+    AND job_location = 'Anywhere'
     AND salary_year_avg IS NOT NULL
 ORDER BY
     salary_year_avg DESC
-LIMIT 1000
+LIMIT 1000;
